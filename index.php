d4896197abc02d4ed22f64c4d5bcad97e983dff1
Ryohei Kondo <standtsukai@gmail.com>
2013-04-30 22:43:07 +0900

    テストできないのでいろいろやってみている

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
 <?php
+
+// クラスを定義@
+class Index
+{
+    private $name;  // 商品名
+    private $price;   // 価格
+
+    // 商品名を取得する
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    // 商品名を設定する
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
